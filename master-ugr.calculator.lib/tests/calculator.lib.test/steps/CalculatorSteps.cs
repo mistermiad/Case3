9ac27791f7eefd6ef675eccb4a1780cecc2585bd
Meead Meyfour Asadi <miadmeyfour@hotmail.es>
2025-01-16 14:11:40 +0100

    revertir string en resultados

--- master-ugr.calculator.lib/tests/calculator.lib.test/steps/CalculatorSteps.cs
+++ master-ugr.calculator.lib/tests/calculator.lib.test/steps/CalculatorSteps.cs
@@ -67,9 +67,9 @@ namespace calculator.lib.test.steps
         [Then(@"the result should be (.*)")]
         [Then(@"the result shall be (.*)")]
         [Then(@"the result is (.*)")]
-        public void ThenTheResultShouldBe(string expectedResult)
+        public void ThenTheResultShouldBe(double expectedResult)
         {
-            var result = _scenarioContext.Get<string>("result");
+            var result = _scenarioContext.Get<double>("result");
          
             Assert.Equal(result, expectedResult);
         }
