9ad77f43902a9b617547bfdd7d22e711e792fb59
Meead Meyfour Asadi <miadmeyfour@hotmail.es>
2025-01-17 21:03:23 +0100

    SquareRoot tests

---
+++ master-ugr.calculator.back-end/tests/calculator.backend.test/steps/SquareRootSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace calculator.lib.test.steps
+{
+    [Binding]
+    public class SquareRootSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        public SquareRootSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+
+        [Given(@"a number (.*) to calculate the Square Root")]
+        public void WhenNumberIsCheckedForSR(int number)
+        {
+            _scenarioContext.Add("number", number);
+        }
+
+
+        [When(@"Square Root is calculated")]
+        public void IGetSquareRoot()
+        {
+            using (var client = new HttpClient())
+            {
+                var urlBase = _scenarioContext.Get<string>("urlBase");
+                var url = $"{urlBase}api/Calculator/";
+                var api_call = $"{url}number_attribute?number={number}";
+                var response = client.GetAsync(api_call).Result;
+                response.EnsureSuccessStatusCode();
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+                var jsonDocument = JsonDocument.Parse(responseBody);
+                var result = jsonDocument.RootElement.GetProperty("sqrt").GetDouble();
+
+                _scenarioContext.Add("getSquareRoot", result);
+            }
+        }
+
+
+        [Then(@"the answer of the square root is (.*)")]
+        public void ItShouldBeSquareRoot(double expectedSquareRoot)
+        {
+            var getSquareRoot = _scenarioContext.Get<double>("getSquareRoot");
+            Assert.Equal(expectedSquareRoot, getSquareRoot);
+        }
+
+    }
+}
